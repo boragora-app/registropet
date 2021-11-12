387cae57c88f870a71944219e8909a30b7462565
Robson Caldas <p25@rcaldas.com>
2021-11-11 21:29:29 -0300

    Modal

--- pet.js
+++ pet.js
@@ -1,1 +1,15 @@
-console.log('ok')
+const modal = document.querySelector(".modal"); //selects the modal
+const btnCloseModal = document.querySelector(".close-modal"); //selects the button to close the modal
+const btnOpenModal = document.querySelector(".show-modal"); //selects the button to show the modal
+const overlay = document.querySelector(".overlay"); //selects the overlay
+
+const toggleModal = function () {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+};
+
+btnOpenModal.addEventListener("click", toggleModal);
+
+btnCloseModal.addEventListener("click", toggleModal);
+
+overlay.addEventListener("click", toggleModal);
